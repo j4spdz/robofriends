c4218a518c2108cadc3574d1d787ada4feeebcd0
j4spdz <code.jack4spadesz@gmail.com>
2020-05-30 10:59:00 +0800

    Added ErrorBoundary as it was missing

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,6 +2,12 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
+
+  //To test ErrorBoundary 
+  // if(true){
+  //   throw new Error("NOOOOOOOO!");
+  // }
+
   return (
     <div>
       {
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,26 +3,27 @@ import { connect } from "react-redux";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
+import ErrorBoundary from "../components/ErrorBoundry";
 import "./App.css";
 
 import { setSearchField, requestRobots } from "../actions";
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     searchField: state.searchRobots.searchField, //come from reducer
     robots: state.requestRobots.robots,
     isPending: state.requestRobots.isPending,
-    error: state.requestRobots.error
-  }
-}
+    error: state.requestRobots.error,
+  };
+};
 
 //dispatch is what triggers an action to reducer
 const mapDispatchToProps = (dispatch) => {
   return {
     onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
-    onRequestRobots: () => dispatch(requestRobots())
-  }
-}
+    onRequestRobots: () => dispatch(requestRobots()),
+  };
+};
 
 class App extends Component {
   componentDidMount() {
@@ -41,7 +42,9 @@ class App extends Component {
         <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={onSearchChange} />
         <Scroll>
-          <CardList robots={filteredRobots} />
+          <ErrorBoundary>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundary>
         </Scroll>
       </div>
     );
